3891f0ada383d2225365c0b8b50d511e2c174101
riicodespretty <rii.codes.pretty@gmail.com>
2025-08-05 12:04:51 +0800

    feat: :recycle: streamline login auth flow

---
+++ src/components/navigation/NavHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <UNavigationMenu
+    highlight
+    highlight-color="primary"
+    orientation="horizontal"
+    :items="items"
+    class="border-default px-6 data-[orientation=horizontal]:w-full data-[orientation=horizontal]:border-b data-[orientation=vertical]:w-48"
+  />
+</template>
+
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue'
+import type { NavigationMenuItem } from '@nuxt/ui'
+import { ref } from 'vue'
+
+const { logout } = useAuth0()
+
+const items = ref<NavigationMenuItem[][]>([
+  [],
+  [
+    {
+      label: 'Logout',
+      icon: 'i-lucide-log-out',
+      class: 'cursor-pointer',
+      onSelect() {
+        logout({
+          logoutParams: {
+            returnTo: window.location.origin,
+          },
+        })
+      },
+    },
+  ],
+])
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,51 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory, type RouteLocation } from 'vue-router'
 
 import Home from '@/views/HomeView.vue'
-import Login from '@/views/LoginView.vue'
-import { useAuth0 } from '@auth0/auth0-vue'
-
-const routes = [
-  { path: '/', name: 'Login', component: Login },
-  { path: '/home', name: 'Home', component: Home },
-]
+import { createAuthGuard, useAuth0 } from '@auth0/auth0-vue'
+import { watchOnce } from '@vueuse/core'
 
 const router = createRouter({
-  history: createMemoryHistory(),
-  routes,
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+      beforeEnter: authGuard,
+    },
+  ],
 })
 
-router.beforeEach(async (to) => {
-  const { checkSession, isAuthenticated } = useAuth0()
-  await checkSession()
+async function authGuard(to: RouteLocation) {
+  return createAuthGuard({
+    redirectLoginOptions: {
+      appState: {
+        target: to.path,
+      },
+    },
+  })(to)
+}
 
-  if (
-    // make sure the user is authenticated
-    !isAuthenticated.value
-    // ❗️ Avoid an infinite redirect
-    && to.name !== 'Login'
-  ) {
-    // redirect the user to the login page
-    return { name: 'Login' }
+router.beforeEach((to, _from, next) => {
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0()
+
+  const authFlow = () => {
+    if (!isAuthenticated.value) {
+      loginWithRedirect({ appState: { targetUrl: to.path } })
+    }
+
+    else next()
+  }
+
+  if (isLoading.value) {
+    watchOnce(isLoading, (loading) => {
+      if (!loading) {
+        authFlow()
+      }
+    })
+  }
+  else {
+    authFlow()
   }
 })
 
